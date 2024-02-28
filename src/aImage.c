4e4b2bf7d3520995b2bebc217f544b8fb544e44a
McCoy1701 <Jkellum819@gmail.com>
2024-02-28 14:42:19 -0500

    added aScreenshot function to aImage.c

--- src/aImage.c
+++ src/aImage.c
@@ -26,3 +26,36 @@ SDL_Surface* aImageLoad( char *filename )
 
   return surf;
 }
+
+int aScreenshot( const char *filename, SDL_Renderer *renderer )
+{
+  SDL_Rect aViewport;
+  SDL_Surface *aSurface = NULL;
+
+  SDL_RenderGetViewport( renderer, &aViewport );
+
+  aSurface = SDL_CreateRGBSurface( 0, aViewport.w, aViewport.h, 32, 0, 0, 0, 0 );
+
+  if ( aSurface == NULL )
+  {
+    printf( "Failed to create surface %s\n", SDL_GetError() );
+    return 0;
+  }
+
+  if ( SDL_RenderReadPixels( renderer, NULL, aSurface->format->format, aSurface->pixels, aSurface->pitch ) != 0 )
+  {
+    printf( "Failed to read pixels from renderer: %s\n", SDL_GetError() );
+    SDL_FreeSurface( aSurface );
+    return 0;
+  }
+
+  if ( IMG_SavePNG( aSurface, filename ) != 0 )
+  {
+    printf( "Failed to save surface as png: %s, %s", filename, SDL_GetError() );
+    SDL_FreeSurface( aSurface );
+    return 0;
+  }
+  
+  SDL_FreeSurface( aSurface );
+  return 1;
+}
